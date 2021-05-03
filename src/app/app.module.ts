06a4091990882d65abdd92f1c9f5b2ab66162519
adeniasfilho <adeniasfilho.n1@gmail.com>
2021-05-03 20:17:35 -0300

    Complemento do código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ClienteInserirComponent } from './clientes/cliente-inserir/cliente-inserir.component';
 import{ MatInputModule} from "@angular/material/input";
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { ClienteListaComponent } from './clientes/cliente-lista/cliente-lista.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 @NgModule({
   declarations: [
     AppComponent,
-    ClienteInserirComponent
+    ClienteInserirComponent,
+    CabecalhoComponent,
+    ClienteListaComponent
   ],
   imports: [
     BrowserModule,FormsModule,
-    MatInputModule,
+    MatInputModule,MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,MatExpansionModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/clientes/cliente-inserir/cliente-inserir.component.ts
+++ src/app/clientes/cliente-inserir/cliente-inserir.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Cliente } from '../cliente.model';
 @Component({
 selector: 'app-cliente-inserir',
 templateUrl: './cliente-inserir.component.html',
+styleUrls: ['./cliente-inserir.component.css'],
 })
-
-
-
-
 export class ClienteInserirComponent {
-    nome: string;
-    fone: string;
-    email: string;
-    onAdicionarCLiente() {
-    console.log('inserindo cliente...');
-    }
+@Output() clienteAdicionado = new EventEmitter<Cliente>();
+nome: string;
+fone: string;
+email: string;
+onAdicionarCLiente() {
+const cliente: Cliente = {
+nome: this.nome,
+fone: this.fone,
+email: this.email,
+};
+this.clienteAdicionado.emit(cliente);
+}
 }
