e25bab513bd6f7439a73d3975f14eccb75dd0a6e
adeniasfilho <adeniasfilho.n1@gmail.com>
2021-05-03 22:02:23 -0300

    Atualizando código

--- src/app/clientes/cliente-lista/cliente-lista.component.ts
+++ src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente.model';
 import { ClienteService } from '../cliente.service';
 @Component({
-selector: 'app-cliente-lista',
-templateUrl: './cliente-lista.component.html',
-styleUrls: ['./cliente-lista.component.css'],
+  selector: 'app-cliente-lista',
+  templateUrl: './cliente-lista.component.html',
+  styleUrls: ['./cliente-lista.component.css'],
 })
 export class ClienteListaComponent implements OnInit {
-clientes: Cliente[] = [];
-constructor(public clienteService: ClienteService) {}
-ngOnInit(): void {
+  clientes: Cliente[] = [];
+  constructor(public clienteService: ClienteService) {}
+  ngOnInit(): void {
     this.clientes = this.clienteService.getClientes();
     this.clienteService
-    .getListaDeClientesAtualizadaObservable()
-    .subscribe((clientes: Cliente[]) => {
-    this.clientes = clientes;
-    });
-    }
+      .getListaDeClientesAtualizadaObservable()
+      .subscribe((clientes: Cliente[]) => {
+        this.clientes = clientes;
+      });
+  }
 }
