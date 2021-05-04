a58288c9b4bf6779fb8062eed93f6230fa3518ef
adeniasfilho <adeniasfilho.n1@gmail.com>
2021-05-04 19:41:32 -0300

    Foi implantado o método OnDestroy()

--- src/app/clientes/cliente-inserir/cliente-inserir.component.ts
+++ src/app/clientes/cliente-inserir/cliente-inserir.component.ts
@@ -4,33 +4,35 @@ import { Cliente } from '../cliente.model';
 import { NgForm } from '@angular/forms';
 import { ClienteService } from '../cliente.service';
 @Component({
-selector: 'app-cliente-inserir',
-templateUrl: './cliente-inserir.component.html',
-styleUrls: ['./cliente-inserir.component.css'],
+  selector: 'app-cliente-inserir',
+  templateUrl: './cliente-inserir.component.html',
+  styleUrls: ['./cliente-inserir.component.css'],
 })
 export class ClienteInserirComponent {
-constructor(public clienteService: ClienteService) {}
-//remover
-@Output() clienteAdicionado = new EventEmitter<Cliente>();
-//remover
-nome: string;
-fone: string;
-email: string;
-onAdicionarCliente(form: NgForm) {
-if (form.invalid) {
-return;
+  constructor(public clienteService: ClienteService) {}
+  //remover
+  @Output() clienteAdicionado = new EventEmitter<Cliente>();
+  //remover
+  nome: string;
+  fone: string;
+  email: string;
+  onAdicionarCliente(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    //remover
+    const cliente: Cliente = {
+      nome: form.value.nome,
+      fone: form.value.fone,
+      email: form.value.email,
+    };
+    this.clienteService.adicionarCliente(
+      form.value.nome,
+      form.value.fone,
+      form.value.email
+    );
+    form.resetForm();
+    //remover
+    this.clienteAdicionado.emit(cliente);
+  }
 }
-//remover
-const cliente: Cliente = {
-nome: form.value.nome,
-fone: form.value.fone,
-email: form.value.email,
-};
-this.clienteService.adicionarCliente(
-form.value.nome,
-form.value.fone,
-form.value.email
-);
-//remover
-this.clienteAdicionado.emit(cliente);
-}}
--- src/app/clientes/cliente-lista/cliente-lista.component.ts
+++ src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Cliente } from '../cliente.model';
 import { ClienteService } from '../cliente.service';
+import { Subscription, Observable } from 'rxjs';
 @Component({
   selector: 'app-cliente-lista',
   templateUrl: './cliente-lista.component.html',
   styleUrls: ['./cliente-lista.component.css'],
 })
-export class ClienteListaComponent implements OnInit {
+export class ClienteListaComponent implements OnInit, OnDestroy {
   clientes: Cliente[] = [];
+  private clientesSubscription: Subscription;
   constructor(public clienteService: ClienteService) {}
+  
   ngOnInit(): void {
     this.clientes = this.clienteService.getClientes();
-    this.clienteService
+    this.clientesSubscription = this.clienteService
       .getListaDeClientesAtualizadaObservable()
       .subscribe((clientes: Cliente[]) => {
         this.clientes = clientes;
       });
+  }
+  ngOnDestroy(): void {
+    this.clientesSubscription.unsubscribe();
   }
 }
